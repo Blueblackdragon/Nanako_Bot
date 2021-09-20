0b1719db6dae0a2b88cd9447da3fe058fa598ef3
Blueblackdragon <doteywarden@gmail.com>
2021-09-19 18:38:46 -0700

    something to play

--- commands/play.js
+++ commands/play.js
@@ -12,15 +12,27 @@ module.exports = {
 		.setName('play')
 		.setDescription('Information about the options provided.')
 		.addStringOption(option => option.setName('input').setDescription('The input to echo back').setRequired(true)),
-	async execute(interaction, nanako, player) {
+	async execute(interaction, nanako, player, queue) {
         const url = interaction.options.getString("input");
         console.log(url);
         let stream = await play.stream(url);
-        const connection = getVoiceConnection(interaction.member.guild.id)
-        //const player = createAudioPlayer();
+        const connection = getVoiceConnection(interaction.member.guild.id);
+
         const resource = createAudioResource(stream.stream, { inputType: stream.type }, { inlineVolume: true });
         connection.subscribe(player);
         player.play(resource);
-        return player, interaction.reply(`Now Playing: ${url}`)
+        console.log(player._state.status)
+            
+            // player.on(AudioPlayerStatus.Idle, () => {
+            //     console.log(player._state.status)
+            //     queue.shift();
+            //     console.log(queue)
+            //     queue.push(shiftEle);
+            //     console.log(shiftEle);
+            //     console.log(queue[0])
+            //     player.play(queue[0]);
+            // })
+
+        return interaction.reply({ content: `Now Playing: ${url}`, ephemeral: true })
 }};
 
